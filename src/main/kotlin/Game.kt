1bec6361c940fa45db4abeb9fbde6619fcab58dc
Rafael Bandeira <rafael.bandeira@live.com>
2021-12-02 21:57:23 -0300

    implement table

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,3 +1,27 @@
-fun main() {
+var board = arrayListOf<ArrayList<String>>()
 
+fun main() {
+    for (x in 0..2) {
+        val row = arrayListOf<String>()
+        for (y in 0..2)
+            row.add("")
+        board.add(row)
+    }
+
+    displayBoard()
+}
+
+fun displayBoard() {
+    println("-------------")
+    for (x in 0..2) {
+        for (y in 0..2) {
+            when (board[x][y]) {
+                "X" -> print("| X ")
+                "O" -> print("| O ")
+                else -> print("|   ")
+            }
+        }
+        println("|")
+        println("-------------")
+    }
 }
