b7d33d7b3995bdec6c5538e36a53aa09a55cd094
Rafael Bandeira <rafael.bandeira@live.com>
2021-12-02 22:14:19 -0300

    implementing player move

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,3 +1,5 @@
+import java.lang.Exception
+
 var board = arrayListOf<ArrayList<String>>()
 
 fun main() {
@@ -9,6 +11,30 @@ fun main() {
     }
 
     displayBoard()
+
+    var continueGame = true
+
+    do {
+        println("Enter position (e.g. 1, 2)")
+        val input = readLine() ?: ""
+        var x = 0
+        var y = 0
+
+        try {
+            val positions = input.split(",")
+            x = positions[0].trim().toInt()
+            y = positions[1].trim().toInt()
+
+            if (board[x-1][y-1].isNotEmpty())
+                println("Position already taken, try again")
+            else {
+                board[x-1][y-1] = "X"
+                displayBoard()
+            }
+        } catch (e: Exception) {
+            println("Invalid input")
+        }
+    } while (continueGame)
 }
 
 fun displayBoard() {
